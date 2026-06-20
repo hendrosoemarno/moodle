8285ed5d237fd8da9523e1e380dbefce2858b1a5
ailearning <admin@ailearning.id>
2026-06-20 11:08:23 +0700

    Add English language file for deepdiagnostic (fix [[stringname]] issue)

---
+++ mod/deepdiagnostic/lang/en/deepdiagnostic.php
@@ -0,0 +1,25 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Deep Diagnostic Report';
+$string['modulename'] = 'Deep Diagnostic Report';
+$string['modulenameplural'] = 'Deep Diagnostic Reports';
+$string['modulename_help'] = 'Displays a diagnostic report of Deep quiz results for the current user.';
+$string['pluginadministration'] = 'Deep Diagnostic Report administration';
+
+$string['deepdiagnostic:addinstance'] = 'Add a new Deep Diagnostic Report';
+$string['deepdiagnostic:view'] = 'View Deep Diagnostic Report';
+
+$string['privacy:metadata'] = 'The Deep Diagnostic Report plugin does not store any personal data.';
+
+$string['deepdiagnosticname'] = 'Report name';
+$string['template_header'] = 'Header Template';
+$string['template_header_help'] = 'Text displayed before the analysis items. Use {studentname} for student name.';
+$string['template_footer'] = 'Footer Template';
+$string['template_footer_help'] = 'Text displayed after the analysis items. Use {studentname} for student name, {levelnum} for level number, and {leveltopic} for level topic.';
+
+$string['status_kompeten'] = 'mastered and competent';
+$string['status_belum_kompeten'] = 'not yet competent';
+$string['status_sebagian'] = 'can do but not yet competent';
+$string['no_quizzes'] = 'No Deep quizzes found in this course.';
+$string['no_data'] = 'No quiz data available to display.';
